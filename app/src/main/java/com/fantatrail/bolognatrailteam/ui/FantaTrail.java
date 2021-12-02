952f8d2b5b5b5c8eab55edbe45b782950dcc75e5
Fabio Salomoni <salomonfabio1920@gmail.com>
2021-12-02 21:19:32 +0100

    ProvaModifica

--- app/src/main/java/com/fantatrail/bolognatrailteam/ui/FantaTrail.java
+++ app/src/main/java/com/fantatrail/bolognatrailteam/ui/FantaTrail.java
@@ -1,6 +1,12 @@
 package com.fantatrail.bolognatrailteam.ui;
 
+import android.os.Bundle;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FantaTrail extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
